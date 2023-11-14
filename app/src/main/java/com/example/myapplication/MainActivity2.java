d0523cd269d31993b5dfe90516f34f65aaa9eea3
mostafizur06 <mostafizur1042@gmail.com>
2023-11-15 01:51:36 +0600

    final

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class MainActivity2 extends AppCompatActivity {
-    TextView outputViewObj;
+    RadioGroup myRadioGroupObj;
+
+    static final  String userMarks="question 1 marks";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        myRadioGroupObj = findViewById(R.id.radioGroupID);
+    }
 
+    public void submitFunction(View view) {
         Intent myGetIntent = getIntent();
-        int result = myGetIntent.getIntExtra(MainActivity.userMarks, 0);
-        outputViewObj = findViewById(R.id.outputViewID);
-        outputViewObj.setText("Your marks is " + result);
+        int mark = myGetIntent.getIntExtra(MainActivity.userMarks, 0);
+
+        if (myRadioGroupObj.getCheckedRadioButtonId() == R.id.radioBtn3Id) {
+            mark += 5;
+        }
+
+        Intent myIntent = new Intent(this, MainActivity3.class);
+        myIntent.putExtra(userMarks, mark);
+        startActivity(myIntent);
     }
 }
